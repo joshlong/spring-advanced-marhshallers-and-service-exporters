0093e949c7eef044d6600aa069cdb96aea4f2d81
Josh Long <josh.long@springsource.com>
2011-08-17 01:17:27 -0700

    adding a new test class.

---
+++ messagepack/src/test/java/org/springframework/remoting/messagepack/Human.java
@@ -0,0 +1,20 @@
+package org.springframework.remoting.messagepack;
+
+/**
+ *
+ */
+public class Human {
+	private String name ;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Human(String name) {
+		this.name = name;
+	}
+}
