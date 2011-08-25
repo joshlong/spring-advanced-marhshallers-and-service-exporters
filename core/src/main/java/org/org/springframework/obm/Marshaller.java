37464c67072847604dec6ef906e5c4ded4889428
Josh Long <josh.long@springsource.com>
2011-08-25 05:48:47 -0700

    ic of some common interfaces

---
+++ core/src/main/java/org/org/springframework/obm/Marshaller.java
@@ -0,0 +1,15 @@
+package org.org.springframework.obm;
+
+
+import org.springframework.oxm.XmlMappingException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public interface Marshaller {
+
+	boolean supports(Class<?> clazz);
+
+	void marshal(Object graph, OutputStream result) throws IOException, XmlMappingException;
+
+}
