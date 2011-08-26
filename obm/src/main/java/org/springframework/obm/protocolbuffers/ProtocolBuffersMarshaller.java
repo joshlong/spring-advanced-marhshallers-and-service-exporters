75a11a93fb538bac19960e01823294878b1cf5a7
Josh Long <josh.long@springsource.com>
2011-08-26 04:36:15 -0700

    moving all the old code. soon itll be deleted, anyway. i just want it so i can port over some of the tests.

--- obm/src/main/java/org/springframework/obm/protocolbuffers/ProtocolBuffersMarshaller.java
+++ obm/src/main/java/org/springframework/obm/protocolbuffers/ProtocolBuffersMarshaller.java
@@ -14,6 +14,7 @@ import java.io.OutputStream;
  * @author Josh Long
  */
 public class ProtocolBuffersMarshaller <T>  extends AbstractMarshaller<T> implements InitializingBean {
+
     @Override
     public void afterPropertiesSet() throws Exception {
 
