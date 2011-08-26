6cd0a864816debaa94761f3162ca0a15bfa29e69
Josh Long <josh.long@springsource.com>
2011-08-26 04:37:02 -0700

    attic

---
+++ attic/core/src/main/java/org/springframework/obm/Unmarshaller.java
@@ -0,0 +1,15 @@
+package org.springframework.obm;
+
+
+import org.springframework.oxm.XmlMappingException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface Unmarshaller {
+
+	boolean supports(Class<?> clazz);
+
+	Object unmarshal(InputStream source) throws IOException, XmlMappingException;
+
+}
