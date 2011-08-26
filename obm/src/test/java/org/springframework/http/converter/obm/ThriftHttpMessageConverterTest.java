0c9f6917468493b407bb26cb74233425a586e5be
Josh Long <josh.long@springsource.com>
2011-08-26 00:33:56 -0700

    adding tests

--- obm/src/test/java/org/springframework/http/converter/obm/ThriftHttpMessageConverterTest.java
+++ obm/src/test/java/org/springframework/http/converter/obm/ThriftHttpMessageConverterTest.java
@@ -49,59 +49,6 @@ public class ThriftHttpMessageConverterTest extends BaseMarshallingHttpMessageCo
     public void testHttpReading() throws Throwable {
         doTestHttpWriting(customer.getClass(), customer);
     }
+
+
 }
-//
-// {
-///*
-//	ThriftHttpMessageConverter thriftMessageConverter = new ThriftHttpMessageConverter();
-//
-//	TDeserializer deserializer = new TDeserializer();
-//
-//	TSerializer serializer = new TSerializer();
-//
-//	MediaType thriftMediaType = ThriftHttpMessageConverter.MEDIA_TYPE;
-//
-//	Customer customer = new Customer("Josh", "Long", "josh@email.com", 242);
-//
-//	@Before
-//	public void before() throws Throwable {
-//		this.thriftMessageConverter.afterPropertiesSet();
-//	}
-//
-//	@Test
-//	public void testHttpWriting() throws Throwable {
-//		Assert.assertTrue("the converter should be able to read the class", thriftMessageConverter.canWrite(Customer.class, thriftMediaType));
-//		HttpHeaders headers = Mockito.mock(HttpHeaders.class);
-//		HttpOutputMessage httpOutputMessage = Mockito.mock(HttpOutputMessage.class);
-//		Mockito.when(httpOutputMessage.getHeaders()).thenReturn(headers);
-//		Mockito.when(headers.getContentType()).thenReturn(this.thriftMediaType);
-//		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//		Mockito.when(httpOutputMessage.getBody()).thenReturn(byteArrayOutputStream);
-//
-//		thriftMessageConverter.write(customer, this.thriftMediaType, httpOutputMessage);
-//		byteArrayOutputStream.flush();
-//		byteArrayOutputStream.close();
-//		byte[] bytesWritten = byteArrayOutputStream.toByteArray();
-//		Customer readCustomer = new Customer();
-//		deserializer.deserialize(readCustomer, bytesWritten);
-//		Assert.assertEquals("the two customers should have the same properties ", this.customer, readCustomer);
-//	}
-//
-//	@Test
-//	public void testHttpReading() throws Throwable {
-//		Assert.assertTrue("the converter should be able to read the class.", thriftMessageConverter.canRead(Customer.class, thriftMediaType));
-//		HttpHeaders headers = Mockito.mock(HttpHeaders.class);
-//		HttpInputMessage inputMessage = Mockito.mock(HttpInputMessage.class);
-//		Mockito.when(headers.getContentType()).thenReturn(this.thriftMediaType);
-//		Mockito.when(inputMessage.getHeaders()).thenReturn(headers);
-//		InputStream fakeBody = fakeInputStream(customer);
-//		Mockito.when(inputMessage.getBody()).thenReturn(fakeBody);
-//		Customer customerResponse = (Customer) this.thriftMessageConverter.read(Customer.class, inputMessage);
-//		Assert.assertEquals(customer, customerResponse);
-//	}
-//
-//	private InputStream fakeInputStream(TBase obj) throws Exception {
-//		byte[] bytesSrc = serializer.serialize(obj);
-//		return new ByteArrayInputStream(bytesSrc);
-//	}*/
-//}
