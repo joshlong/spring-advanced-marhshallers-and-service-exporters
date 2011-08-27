fe8fda062b5a8ef55108252120c6bf0805798323
Josh Long <josh.long@springsource.com>
2011-08-27 05:35:13 -0700

    added support for the snappy compression library, fixed missing licenses, added more tests.

--- obm/src/main/java/org/springframework/obm/snappy/SnappyMarshaller.java
+++ obm/src/main/java/org/springframework/obm/snappy/SnappyMarshaller.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.obm.snappy;
 
 import org.springframework.obm.Marshaller;
@@ -13,8 +29,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * This marshaller is a wrapping {@link org.springframework.obm.Marshaller}, meant to be used in conjunction with
- * any other {@link org.springframework.obm.Marshaller}.
+ * <P> This marshaller is a wrapping {@link org.springframework.obm.Marshaller}, meant to delegate actual
+ * serialization duties to another {@link org.springframework.obm.Marshaller}.
+ * <P>  The Snappy library (http://code.google.com/p/snappy-java) is a compression library from Google that is optimized for speed and modest comppression.
+ * <P> While Snappy is a C++ library, C bindings are provided and that C binding is wrappd
+ * by the Snappy-Java project, which consumes the C bindings through JNI. There are 32 and 64 bit builds packaged with the Snappy library for OSX, Linux, and Windows.
+ * You should not need to do anything to use this (not even setup <CODE>LD_PATH</CODE> or <CODE>library.path</CODE>, but you
+ * should be aware that the native code is there. It has been testd and works correctly in multi-classloader environments like Tomcat
+ * </p>
  *
  * @author Josh Long
  */
